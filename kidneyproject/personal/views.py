0334adcacba597d726a75ed043d0dc67b0002bbe
nbain00 <zink47@gmail.com>
2022-11-28 13:58:16 -0700

    Added view functions

--- kidneyproject/personal/views.py
+++ kidneyproject/personal/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def indexPageView(request) :
     return render(request, 'base.html')
+
+def foodJournalView(request) :
+    return HttpResponse('This will be the food journal page')
+
+def addFoodView(request) :
+    return HttpResponse('Page for adding new food')
+
+def levelsLogView(request) :
+    return HttpResponse('This page will have serum levels info')
+
+def addLevelView(request) :
+    return HttpResponse('Page for adding serum levels')
