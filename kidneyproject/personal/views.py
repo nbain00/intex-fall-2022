e74d75da61cd1f8a6a2f51f63edfc511c93df18f
nbain00 <zink47@gmail.com>
2022-11-29 16:11:13 -0700

    add food form

--- kidneyproject/personal/views.py
+++ kidneyproject/personal/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import SerumLevelLog
 from .models import Patient
 from .models import SerumType
+from .models import Food
 
 # Create your views here.
 def indexPageView(request) :
@@ -13,7 +14,32 @@ def foodJournalView(request) :
     return render(request, 'personal/journal.html')
 
 def addFoodView(request) :
-    return render(request, 'personal/food.html')
+    if request.method == 'POST' :
+        food = Food()
+
+        food.measurement_type = request.POST['measurement_type']
+        food.name = request.POST['name']
+        food.calories_kj = request.POST['calories_kj']
+        food.water_g = request.POST['water_g']
+        food.protein_g = request.POST['protein_g']
+        food.total_fat_g = request.POST['total_fat_g']
+        food.total_fiber_g = request.POST['total_fiber_g']
+        food.alcohol_g = request.POST['alcohol_g']
+        food.total_sugars_g = request.POST['total_sugars_g']
+        food.added_sugars_g = request.POST['added_sugars_g']
+        food.total_carbs_g = request.POST['total_carbs_g']
+        food.ca_g = request.POST['ca_g']
+        food.phos_g = request.POST['phos_g']
+        food.k_g = request.POST['k_g']
+        food.na_g = request.POST['na_g']
+        food.total_saturated_fat_g = request.POST['total_saturated_fat_g']
+        food.total_unsaturated_fat_g = request.POST['total_unsaturated_fat_g']
+
+        food.save()
+
+        return HttpResponse('Food added successfully')
+    else:
+        return render(request, 'personal/food.html')
 
 def levelsLogView(request) :
     data = SerumLevelLog.objects.all()
