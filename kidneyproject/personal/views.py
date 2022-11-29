8fc0eb86d1a045691e2b6140328e6448cde0fe31
nbain00 <zink47@gmail.com>
2022-11-29 13:46:17 -0700

    changed models

--- kidneyproject/personal/views.py
+++ kidneyproject/personal/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Patient
 
 # Create your views here.
 def indexPageView(request) :
+    data = None
     return render(request, 'personal/index.html')
 
 def foodJournalView(request) :
