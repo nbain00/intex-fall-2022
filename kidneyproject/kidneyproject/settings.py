ac6e8d0f6a4f7c057be343a984977b3a315816c3
nbain00 <zink47@gmail.com>
2022-12-01 15:58:52 -0700

    for deployment

--- kidneyproject/kidneyproject/settings.py
+++ kidneyproject/kidneyproject/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lb74&2hy=k=td+!n6v^=w&e=s$*(784z3jv9p@64_z+oa#qj+h'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
