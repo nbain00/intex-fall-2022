651ebcb305c0910892ba70832c9a6ee66c656b9f
bruyndecker <bruyn.decker@byu.edu>
2022-12-01 10:52:30 -0700

    forms file, also added info to home page

--- kidneyproject/kidneyproject/settings.py
+++ kidneyproject/kidneyproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'personal.apps.PersonalConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,7 @@ DATABASES = {
         'USER' : 'postgres',
         'PASSWORD' : 'admin',
         'HOST' : 'localhost',
-        'PORT' : 5433,
+        #'PORT' : 5433,
     }
 }
 
@@ -127,3 +128,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
